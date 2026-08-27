9dd977dd102789c982fbfb6ca5bf56b9ff2bfec7
Karla Famula <karlafamula@gmail.com>
2026-08-26 21:22:55 -0300

    feat Integração na MainActivity

---
+++ app/src/main/java/karlafamula/com/github/fiap_to_do_list_project/data/Tarefa.kt
@@ -0,0 +1,14 @@
+package karlafamula.com.github.fiap_to_do_list_project.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tarefas")
+data class Tarefa(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val titulo: String,
+    val descricao: String,
+    val concluida: Boolean = false,
+    val dataCriacao: Long = System.currentTimeMillis(),
+    val dataHora: Long? = null
+)
